08a70f02d860e48b0f2cf63630ca1afa3bba10e1
Scott Hill <scott289@gmail.com>
2023-09-13 12:49:10 -0700

    new file

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import {PhotoProps, Photo} from './Photo'
-
-interface iFetchData {
-  error: any,
-  data: any,
-}
-
-export const useFetchData = (url: string) => {
-  const [data, setData] = useState<iFetchData>({
-    error: null,
-    data: null,
-  });
-
-  useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json()
-      })
-      .then((data) => {
-        setData({
-          error: null,
-          data
-        });
-      })
-      .catch((err: Error) => {
-        setData({
-          data: null,
-          error: err
-        });
-      });
-  }, []);
-  return data;
-}
+import { useFetchData } from './hooks';
 
 function App() {
   let { error, data } = useFetchData(`https://picsum.photos/v2/list`);
